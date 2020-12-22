b9482a155c2da2d7522f4e62afd4e34323d17e52
Maciej Rapacz <mmrapacz@protonmail.ch>
2020-12-22 18:26:01 +0100

    Add g

--- fish/config.fish
+++ fish/config.fish
@@ -4,5 +4,6 @@ set TERM screen-256color
 alias gst "git status"
 alias gp "git push origin HEAD"
 alias gpof "git push origin HEAD --force-with-lease"
+alias g "clear; git status"
 
 abbr -a mvl move-last-download
