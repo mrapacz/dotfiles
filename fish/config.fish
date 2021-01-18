9ee9e5b8106b3b57abc6c39346076916774c6379
Maciej Rapacz <mmrapacz@protonmail.ch>
2021-01-18 16:59:22 +0100

    eval conda conditionally

--- fish/config.fish
+++ fish/config.fish
@@ -4,13 +4,15 @@ set TERM screen-256color
 alias gst "git status"
 alias gp "git push origin HEAD"
 alias gpof "git push origin HEAD --force-with-lease"
+alias gpom "git pull origin master"
 alias g "clear; git status"
 alias gd "git diff"
 
 abbr -a mvl move-last-download
 
 # >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval /Users/mrapacz/opt/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+set conda_path ~/opt/anaconda3/conda
+if test -e $conda_path
+    eval $conda_path "shell.fish" "hook" $argv | source
+end
 # <<< conda initialize <<<
-
